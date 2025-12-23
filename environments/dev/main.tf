daa7dd03238670b0e1a91cd58083123b86b1984d
sivasankardevops2025 <sivasankardevops2025@gmail.com>
2025-12-23 12:32:14 +0530

    main file modified

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source        = "envirnoments../../modules/vpc"
+  source        = "../../modules/vpc"
   vpc-cidr      = "10.0.0.0/16"
   subnet-cidr   = "10.0.0.1/24"
   subnet-az     = "${var.aws_region}a"
@@ -7,7 +7,7 @@ module "vpc" {
 } 
 
 module "ec2" {
-  source        = "envirnoments../../modules/ec2"
+  source        = "../../modules/ec2"
   ami_id        = "ami-05f071c65e32875a8"
   instance_type = "t3.micro"
   subnet_id     = module.vpc.subnet_id
